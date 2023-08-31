c2f4eda4e577ae4fe9162c956162abe9e212aca3
dasdfadssda <quddkflty@gmail.com>
2023-08-31 13:51:39 +0900

    add 1st date code

--- 1주차/add.c
+++ 1주차/add.c
@@ -2,7 +2,19 @@
 
 int main()
 {
-    
+    int a = 0;
+    int b = 0;
+
+    // read two numbers from the user into two a and b
+    printf("Input two numbers :");
+    scanf("%d %d", &a, &b);
+
+    // add a and b, and store the result into another variable c
+    int c = 0;
+    c = a + b;
+    // display the result in c
+    printf("result : ");
+    printf("%d + %d = %d\n",a,b,c);
 
     return 0;
 }
