c6d42728eb23db78f0bb6025c6a244c139b8f3d0
dasdfadssda <quddkflty@gmail.com>
2023-09-23 20:14:24 +0900

    edit code:

--- 4주차/hw2_2_22100294.c
+++ 4주차/hw2_2_22100294.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int year, month, day;
-    
+
     // Get input from the user
-    printf("Input your birthday year: ");
+    printf("Input your birthday\n");
+
+    printf("year: ");
     scanf("%d", &year);
-    
-     // Get input from the user
+
+    // Get input from the user
     printf("month: ");
     scanf("%d", &month);
-    
+
     // Get input from the user
     printf("day in month: ");
     scanf("%d", &day);
-    
-    char birthdayNum[7];
-    
+
+    char birthdayNum[32] = "Hello,World!\n";
+
     // save year
-    birthdayNum[0] = (year / 100) % 100 + '0';
-    birthdayNum[1] = (year % 100) / 10 + '0';
-    birthdayNum[2] = (year % 10) + '0';
-    
+    birthdayNum[0] = (year % 100) / 10 + '0';
+    birthdayNum[1] = (year % 10) + '0';
+
     // save month
-    birthdayNum[3] = (month / 10) + '0';
-    birthdayNum[4] = (month % 10) + '0';
-    
+    birthdayNum[2] = (month / 10) + '0';
+    birthdayNum[3] = (month % 10) + '0';
+
     // save day
-    birthdayNum[5] = (day / 10) + '0';
-    birthdayNum[6] = (day % 10) + '0';
-    
+    birthdayNum[4] = (day / 10) + '0';
+    birthdayNum[5] = (day % 10) + '0';
+
     // add null
-    birthdayNum[7] = '\0';
-    
+    birthdayNum[6] = '\0';
+
     // result
     printf("birthday in YYMMDD format = %s\n", birthdayNum);
-    
+
     return 0;
 }
