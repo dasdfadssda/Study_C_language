96e02bb8f5dc9fa9d3583413df3dd56847d402fe
dasdfadssda <quddkflty@gmail.com>
2023-10-26 13:47:13 +0900

    파일 입출력 연습

--- 9주차/file_example2.c
+++ 9주차/file_example2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define FILE_NAME "my_file.txt"
 
@@ -7,6 +8,11 @@ int main()
 
     // open a file
     FILE *fp = fopen(FILE_NAME, "r"); // 읽을 때는 r
+    if (fp == NULL)
+    {
+        printf("Failed to open file %s\n", FILE_NAME); // 에러 체크
+        exit(-1);
+    }
 
     // write data into file
     for (int i = 0; i <= 10; i++)
@@ -18,6 +24,6 @@ int main()
     printf("\n");
 
     // close the file
-
+    fclose(fp); // 중요하다.
     return 0;
 }
---
+++ 9주차/randomGeneration.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define FILE_NAME "random.txt"
+
+int main()
+{
+    srand(time(NULL));
+
+    // open a file
+    FILE *fp = NULL;
+    fp = fopen("random.txt", "w");
+
+    // write data into file
+    for (int i = 0; i <= 10; i++)
+        fprintf(fp, "%d", rand() % 100); // 파일에 입력.
+    fprintf(fp, "\n");
+
+    // close a file
+    fclose(fp);
+    return 0;
+}
