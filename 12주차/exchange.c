6feadeb9d31363ae64d9a3a786eff8c7fd7923d8
dasdfadssda <quddkflty@gmail.com>
2023-11-16 13:49:02 +0900

    포인터 활용

--- 12주차/exchange.c
+++ 12주차/exchange.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
-int main(){
+
+void exchange(int *x, int *y);
+
+int main()
+{
+    int a = 10, b = 20;
+
+    // Input Num
+    printf("숫자 2개를 입력해주세요.\n");
+    scanf("%d %d",&a,&b);
+
+    exchange(&a, &b);
+
+    printf("바꾼 후\n");
+    printf("a = %d, b = %d\n", a, b);
 
     return 0;
+}
+
+void exchange(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    return;
 }
