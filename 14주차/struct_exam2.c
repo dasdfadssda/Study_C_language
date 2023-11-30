6207f31a49cdc3825e0a33080b28afee5d9ce912
dasdfadssda <quddkflty@gmail.com>
2023-11-30 14:12:39 +0900

    structure 구조

--- 14주차/struct_exam2.c
+++ 14주차/struct_exam2.c
@@ -5,33 +5,35 @@
 //     int y;
 // };
 
-typedef struct {
+typedef struct
+{
     int x, y;
 } Point;
 
 // define a function Distance(p1, p2) that returns the distance between two Points p1 and p2
-    // distance = sqrt( (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y))
-    // sqrt() is declared in math.h
+// distance = sqrt( (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y))
+// sqrt() is declared in math.h
 
 double Distance(Point p1, Point p2)
 {
-    return sqrt( (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
 }
 
 // declare a structure 'Size' to represent horizontal and vertical sizes
-    // composed of two integer fields width and height
+// composed of two integer fields width and height
 
 // struct Size {
-//     int width, height;  
+//     int width, height;
 // };
 
-typedef struct {
-    int width, height;  
+typedef struct
+{
+    int width, height;
 } Size;
 
 // define a function Difference(p1, p2)
-    // that returns the difference between them in 'Size' type
-    // e.g., struct Size s = Difference(p1, p2);
+// that returns the difference between them in 'Size' type
+// e.g., struct Size s = Difference(p1, p2);
 
 Size Difference(Point p1, Point p2)
 {
