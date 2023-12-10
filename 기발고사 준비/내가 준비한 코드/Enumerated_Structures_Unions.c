029aa651c92ba33618e019703ee2be84b87c15d2
dasdfadssda <quddkflty@gmail.com>
2023-12-11 00:49:30 +0900

    기말 준비

--- 기발고사 준비/내가 준비한 코드/Enumerated_Structures_Unions.c
+++ 기발고사 준비/내가 준비한 코드/Enumerated_Structures_Unions.c
@@ -1,22 +1,27 @@
 // Typedef :
 #include <stdio.h>
 
-// typedef를 사용하여 기존의 자료형에 새로운 이름을 지정할 수 있습니다.
+// typedef를 사용하여 int라는 기존의 자료형에 INTEGER라는 새로운 이름을 지정합니다.
+// 이렇게 하면 INTEGER를 int처럼 사용할 수 있게 됩니다.
 typedef int INTEGER;
 
 int main()
 {
-    INTEGER a = 5; // INTEGER라는 새로운 이름의 자료형을 사용하여 변수 a를 선언하고 초기화합니다.
+    // INTEGER라는 새로운 이름의 자료형을 사용하여 변수 a를 선언하고 초기화합니다.
+    // INTEGER는 실제로는 int 형이므로, int형 변수를 선언하는 것과 동일합니다.
+    INTEGER a = 5;
 
-    printf("a의 값: %d", a);
+    printf("a의 값: %d", a);  // a의 값을 출력합니다.
 
     return 0;
 }
 
+
 // Enumerated type:
 #include <stdio.h>
 
-// enum을 사용하여 열거형을 선언할 수 있습니다.
+// enum을 사용하여 요일을 나타내는 열거형 DAY를 선언합니다.
+// 각 요일은 SUN부터 SAT까지로, 각각 0부터 6의 값을 갖습니다.
 enum DAY
 {
     SUN,
@@ -30,28 +35,33 @@ enum DAY
 
 int main()
 {
-    enum DAY today = WED; // 열거형 DAY의 값 WED를 today라는 변수에 저장합니다.
+    // 열거형 DAY의 값 WED를 today라는 변수에 저장합니다.
+    // 이 때, WED는 3이므로 today의 값은 3이 됩니다.
+    enum DAY today = WED;
 
-    printf("오늘은 요일 코드: %d", today);
+    printf("오늘은 요일 코드: %d", today);  // today의 값을 출력합니다. 출력값은 3이 됩니다.
 
     return 0;
 }
 
+
 // Structures:
 #include <stdio.h>
 
-// struct를 사용하여 구조체를 선언할 수 있습니다.
+// struct를 사용하여 사람의 이름과 나이를 저장하는 구조체 Person을 선언합니다.
+// 구조체는 서로 다른 타입의 변수들을 묶어서 하나의 새로운 타입을 만드는 것입니다.
 struct Person
 {
-    char name[20]; // 구조체의 멤버로 문자 배열 name을 선언합니다.
-    int age;       // 구조체의 멤버로 정수 age를 선언합니다.
+    char name[20]; // 사람의 이름을 저장하는 문자 배열입니다.
+    int age;       // 사람의 나이를 저장하는 정수 변수입니다.
 };
 
 int main()
 {
-    struct Person p1 = {"John", 30}; // Person 구조체의 인스턴스 p1을 선언하고 초기화합니다.
+    // Person 구조체의 인스턴스 p1을 선언하고, 이름과 나이를 초기화합니다.
+    struct Person p1 = {"John", 30};
 
-    printf("이름: %s, 나이: %d", p1.name, p1.age);
+    printf("이름: %s, 나이: %d", p1.name, p1.age);  // p1의 이름과 나이를 출력합니다.
 
     return 0;
 }
@@ -59,7 +69,8 @@ int main()
 // Unions:
 #include <stdio.h>
 
-// union을 사용하여 공용체를 선언할 수 있습니다.
+// union을 사용하여 int, float, char[20]을 저장할 수 있는 공용체 Data를 선언합니다.
+// 공용체는 여러 멤버 중 하나만 값을 저장할 수 있는 구조체와 같은 자료형입니다.
 union Data
 {
     int i;
@@ -69,13 +80,14 @@ union Data
 
 int main()
 {
-    union Data data; // Data 공용체의 인스턴스 data를 선언합니다.
+    // Data 공용체의 인스턴스 data를 선언합니다.
+    union Data data;
 
-    data.i = 10;
-    printf("data.i: %d\n", data.i);
+    data.i = 10;  // data의 int 멤버 i에 10을 저장합니다.
+    printf("data.i: %d", data.i);  // data의 int 멤버 i의 값을 출력합니다.
 
-    data.f = 220.5;
-    printf("data.f: %.1f", data.f);
+    data.f = 220.5;  // data의 float 멤버 f에 220.5를 저장합니다.
+    printf("data.f: %.1f", data.f);  // data의 float 멤버 f의 값을 출력합니다.
 
     return 0;
 }
